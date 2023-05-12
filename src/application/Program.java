1a134789801f96935a518427a230982fbdc05c3c
JoseCarlos67 <carlos6767jose@gmail.com>
2023-05-12 10:25:56 -0300

    Program and GameFrame

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import view.GameFrame;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		GameFrame window = new GameFrame();
+
+	}
+
+}
---
+++ src/view/GameFrame.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class GameFrame {
+
+	public GameFrame() {
+		JFrame window = new JFrame("JTetris");
+		window.setSize(500, 750);
+		window.setVisible(true);
+	}
+	
+}
